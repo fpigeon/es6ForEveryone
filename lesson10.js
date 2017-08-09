a99cacea29ab68dde344f0b223cfae073e7fa0b2
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-08-09 09:33:05 -0500

    completes lesson 10

---
+++ lesson10.js
@@ -0,0 +1,24 @@
+// const person = {
+//   points: 23,
+//   score() {
+//     this.points++
+//   }
+// }
+// console.log(person)
+// person.score()
+// person.score()
+// person.score()
+// person.score()
+
+// console.log(person)
+
+class Car {
+  constructor(make, color) {
+    this.make = make
+    this.color = color
+  }
+
+}
+
+const beemer = new Car('bwm', 'gold')
+console.log(beemer)
